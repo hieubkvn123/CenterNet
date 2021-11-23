438c1e8d0424122ece353bb20e64ff51f9444b6f
Nong Minh Hieu <hieubkvn123@gmail.com>
2021-11-23 17:03:26 +0800

    ahihi

--- test.py
+++ test.py
@@ -5,14 +5,25 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
+from argparse import ArgumentParser
 from gluoncv import model_zoo, data, utils
 
-center_net_model = 'center_net_resnet101_v1b_dcnv2_coco'
-yolo_model = 'yolo3_darknet53_coco'
+parser = ArgumentParser()
+parser.add_argument('--input', required=True, help='Path to input test image')
+parser.add_argument('--model', required=False, default='center', help='Detection model used')
+args = vars(parser.parse_args())
 
-net = model_zoo.get_model(yolo_model, pretrained=True)
+models = {
+    'center' : 'center_net_resnet101_v1b_dcnv2_coco',
+    'yolo' : 'yolo3_darknet53_coco'
+}
 
-img = cv2.imread('/home/hieu/Pictures/traffic/1.jpg')
+if(args['model'] not in models.keys()):
+    raise Exception('Invalid model name')
+
+net = model_zoo.get_model(models[args['model']], pretrained=True)
+
+img = cv2.imread(args['input'])
 H, W, C = img.shape
 
 start = time.time()
@@ -21,7 +32,8 @@ x = mx.nd.array(img.reshape(H, W, C))
 x, img = data.transforms.presets.center_net.transform_test(x, short=512)
 class_IDS, scores, bounding_boxes = net(x)
 end = time.time()
-print(end - start)
+
+print(f'[INFO] FPS = {1/(end-start)}') 
 
 ax = utils.viz.plot_bbox(img, bounding_boxes[0], scores[0], class_IDS[0], class_names=net.classes)
 plt.show()
